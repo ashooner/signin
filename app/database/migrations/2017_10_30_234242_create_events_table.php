ea1c16514e880e3cbe03e0f06448dfc74d8e178a
jessibreen <jessibreen@gmail.com>
2018-02-27 19:39:30 -0500

    created forms and added new main page

---
+++ app/database/migrations/2017_10_30_234242_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        // Create table for storing roles
+        Schema::create('events', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 75);
+            $table->date('date');
+            $table->string('description', 300);
+            $table->string('type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
---
+++ app/resources/views/events/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+@section('content')
+
+    <h1 class="text-center">Events</h1>
+
+    <hr>
+
+
+    <div class="row">
+
+
+        <div class="col-md-6 col-md-offset-3">
+
+   {{--@foreach ($events as $event)
+    <div>
+        <a href="/events/{{$event->id}}">{{ $event->name}}</a>
+    </div>
+    @endforeach--}}
+
+        <ul class="list-group">
+            @foreach ($events as $event)
+                <li class="list-group-item"><a href="{{url('/events/'.$event->id)}}">{{ $event->name}}</a></li>
+            @endforeach
+        </ul>
+
+
+        </div>
+
+
+        <div class="col-sm-6 col-sm-offset-5">
+            <a href="{{url('/events/new')}}" class="btn btn-lg btn-success">Add New Event</a>
+        </div>
+
+    </div>
+
+@stop
