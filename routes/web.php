b0f1fda6f46968b3fbde9e51b767fdc3c984e7dc
jessibreen <jessibreen@gmail.com>
2018-03-05 23:03:44 -0500

    Change route paths

--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,18 @@ Route::get('/', 'HomeController@index')->name('home');
 
 //Route::get('/', 'EventController@index');
 
-Route::get('/admin/events', 'EventAdminController@index')->name('admin.events');
-Route::get('/admin/events/new', 'EventAdminController@create')->name('admin.events.create');
-Route::post('/admin/events/new', 'EventAdminController@store');
-Route::get('/admin/events/{event}', 'EventAdminController@show')->name('admin.events.show');
-Route::get('/admin/events/{event}/download', 'EventAdminController@download');
+Route::get('admin/events', 'EventAdminController@index')->name('admin.events');
+Route::get('admin/events/new', 'EventAdminController@create')->name('admin.events.create');
+Route::post('admin/events/new', 'EventAdminController@store');
+Route::get('admin/events/{event}', 'EventAdminController@show')->name('admin.events.show');
+Route::get('admin/events/{event}/download', 'EventAdminController@download');
 
-Route::delete('/admin/attendees/{attendee}/delete', 'AttendeeAdminController@destroy')->name('admin.attendees.delete');
+Route::delete('admin/attendees/{attendee}/delete', 'AttendeeAdminController@destroy')->name('admin.attendees.delete');
 
-Route::get('/events', 'EventController@index')->name('events');
+Route::get('events', 'EventController@index')->name('events');
 Route::get('events/{event}', 'EventController@show');
 
 //Route::post('/events/{event}/attendees', 'AttendeeController@store');
-Route::get('/events/{event}/signin', 'SigninController@create');
-Route::post('/events/{event}/signin', 'SigninController@store');
+Route::get('events/{event}/signin', 'SigninController@create');
+Route::post('events/{event}/signin', 'SigninController@store');
 
