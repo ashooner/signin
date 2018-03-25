dd58db5c95dbebac479551eef8db5677a76887d8
jessibreen <jessibreen@gmail.com>
2018-03-25 16:14:16 -0400

    login redirects

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-     protected $redirectTo = '/signin/signin/foo';
+     protected $redirectTo = 'admin/events/new';
 
 
 
