964661389e728b5ef2380d9b52f9e23e0116dd03
jessibreen <jessibreen@gmail.com>
2018-03-25 15:18:42 -0400

    login redirects

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -27,7 +27,7 @@ class RedirectIfAuthenticated
             if (Auth::guard($guard)->check()) {
                 return redirect()->route('home');
             } else {
-                return redirect('/signin/login');
+                return redirect('/login');
             }
 
         } else {
